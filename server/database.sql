e41c28b3e90ecd786838a2aaa0406c80e4f06030
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-06-01 19:50:18 +0100

    Initial Set up

---
+++ server/database.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE team-work;
+
+CREATE TABLE users (
+    Id SERIAL NOT NULL PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    gender VARCHAR(255),
+    jobRole VARCHAR(255),
+    department VARCHAR(255),
+    isAdmin BOOLEAN NOT NULL DEFAULT false,
+    address VARCHAR(255));
+
+
+--Insert fake users
+     INSERT INTO users (firstname,
+     lastname,
+    email,
+    password,
+     gender,
+      jobRole,
+    department,
+    isAdmin,
+     address)
+      VALUES ('goodness', 'aig', 'goodness@gmail.com','password', 'male', 'None', 'develope', true ,'No 7 eyaen, benin city');
