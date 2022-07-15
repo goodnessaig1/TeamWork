0de3457815b672fb45d738bdfd07e631c85de9ef
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-07-15 17:50:56 +0100

    added heroku config info

--- server/models/db.js
+++ server/models/db.js
@@ -1,22 +1,21 @@
-var pg = require("pg");
-
+var pg = require('pg');
 
 var client = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "team",
-    password: "osemudiame1",
-    port: 5000,
-});;
-client.connect(function(err) {
+  user: 'postgres',
+  host: 'localhost',
+  database: 'team',
+  password: 'osemudiame1',
+  port: 5000,
+});
+client.connect(function (err) {
   if (err) {
-    return console.error("could not connect to postgres", err);
+    return console.error('could not connect to postgres', err);
   }
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+  client.query('SELECT NOW() AS "theTime"', function (err, result) {
     if (err) {
-      return console.error("error running query", err);
+      return console.error('error running query', err);
     }
-    console.log("Database Connected");
+    console.log('Database Connected');
   });
 });
 
