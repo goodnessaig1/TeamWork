bac1d7789f52728c7890b3bcb96e667fd9f14722
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-07-22 09:49:30 +0100

    make it better

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const gifRoutes = require('./server/routes/gifRoute');
 const categoryRoutes = require('./server/routes/categoryRoute');
 const articeRoute = require('./server/routes/articleRoute');
 
+const { resolve } = require('path');
+
 const http = require('http');
 
 const app = express();
@@ -27,6 +29,22 @@ app.get('/gifs', (req, res) => {
 });
 
 //       MIDDLEWARES
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
+app.use(express.static(resolve(__dirname, 'src/public')));
+
 app.use(express.json());
 app.use(cors());
 
