adba622b87cf34d2021f502552d43857ea417277
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-09-07 18:33:44 +0100

    make it better

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(resolve(__dirname, 'src/public')));
-app.use(fileupload({ useTempFiles: true }));
+// app.use(fileupload({ useTempFiles: true }));
 app.use(express.json());
 app.use(cors());
 
