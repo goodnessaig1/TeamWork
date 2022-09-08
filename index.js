932802b625a3823aabf6f01042ca754807576e21
goodnessaig1 <98638330+goodnessaig1@users.noreply.github.com>
2022-09-08 07:00:49 +0100

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const users = require('./server/routes/adminRoute');
 const gifRoutes = require('./server/routes/gifRoute');
 const categoryRoutes = require('./server/routes/categoryRoute');
 const articeRoute = require('./server/routes/articleRoute');
-// const formidableMiddleware = require('express-formidable');
 
 const { resolve } = require('path');
 
@@ -12,7 +11,6 @@ const http = require('http');
 
 const app = express();
 const cors = require('cors');
-// app.use(formidableMiddleware());
 
 // ======== SWAGGER DOCUMENTATION ROUTE
 
